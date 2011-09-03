5b6dcae690690e383dd51509811172fd824a740e
Felipe Teixeira <felipe.g.te@gmail.com>
2011-09-03 16:39:59 -0300

    Cleaning the house

--- ADI/libs/Threading/LoadTestCase.js
+++
@@ -1,6 +0,0 @@
-﻿
-var i = 0;
-var limite = 9999999;
-while (i < limite) { ++i; }
-console.info("-------------------------------------------------------------------------------------");
-console.info("elapsed: " + limite + " - running:" + currentNode);
--- ADI/libs/Threading/WorkerTestCase.js
+++
@@ -1,6 +0,0 @@
-﻿
-onmessage = function (event) {
-    var i = event.data * event.data;
-    while (i < 100000000) { ++i; }
-    postMessage(i);
-}
